cecb4605ba01f74667a1c150430dc55f66d6b5c0
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-04-22 09:40:24 +0700

    Implemented activity 2

--- SC05-Express/activity/index.js
+++ SC05-Express/activity/index.js
@@ -4,9 +4,24 @@ const express = require('express');
 // Create an instance of express app
 const app = express();
 
+// Serve static files in directory 'public'
+app.use(express.static('public'));
+
 // Define a root route
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send(`
+    <html>
+        <head>
+        <link rel="stylesheet" href="/css/styles.css">
+        </head>
+
+        <body>
+            <h1>Hello, World</h1>
+            <img src="/images/sample.png" alt="Sample Image">
+            <script src="/js/script.js"></script>
+        </body>
+    </html>
+    `);
 });
 
 // Start the server and listen on port 3000
