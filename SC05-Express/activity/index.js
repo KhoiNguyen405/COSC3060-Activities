ba7af0c1fe8ac05bc50858b1cdf07ede13682ae3
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-04-22 10:05:37 +0700

    Implemented activity 6

--- SC05-Express/activity/index.js
+++ SC05-Express/activity/index.js
@@ -10,6 +10,9 @@ app.use(express.static('public'));
 // Use EJS as view engine
 app.set('view engine', 'ejs');
 
+// Middleware to parse POST data
+app.use(express.urlencoded({ extended: true }));
+
 // JS Object for user data
 let userData = {
     name: "John Doe",
@@ -36,6 +39,23 @@ app.get('/', (req, res) => {
 
     res.render('layout', { user: userData });
 });
+
+// Define a form route
+app.get('/form', (req, res) => {
+    res.render('form');
+});
+
+// Handle POST data
+app.post('/submit', (req, res) => {
+    // Capture POST data
+    const userData = {
+        name: req.body.name,
+        email: req.body.email
+    };
+
+    // Render the success template and pass user data
+    res.render('success', { user: userData });
+})
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
