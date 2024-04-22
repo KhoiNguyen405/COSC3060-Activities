ec1ee84bdcc9ed200dc939845768089764f68057
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-04-22 10:48:27 +0700

    Initialized project for bonus challenge activity 9

--- SC05-Express/rmit-store-starting-materials/index.js
+++ SC05-Express/rmit-store-starting-materials/index.js
@@ -1,5 +1,26 @@
 // This load the products as a javascript object from products.js file
 const products = require('./products');
-console.log(products);
+// console.log(products);
 
 // Fill in your code here to build NodeJS + Express + EJS web application
+// Import express framework
+const express = require('express');
+
+// Create an instance of express app
+const app = express();
+
+// Use EJS as view engine
+app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static('assets'));
+
+// Root
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
