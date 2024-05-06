f4caad9f6bc1195a64dd4ad5400cb0a5ff14fb52
Khoi Nguyen <s4052683@rmit.edu.vn>
2024-05-06 10:12:12 +0700

    Defined Mongoose Schemas and models for export.

--- SC09-MongoDB/part-2/exercise-1/model.js
+++ SC09-MongoDB/part-2/exercise-1/model.js
@@ -5,38 +5,80 @@ mongoose.connect('mongodb+srv://khoinguyen:activitiesDBpassword@activitiescluste
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.log(error.message));
 
-//  Implement the Schema in Mongoose
+/** Schemas for discussion forum related collections **/
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+    date: { type: Date, required: true }, // Date of account creation
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }],
+    admin: { type: Boolean, required: true } // True if user is an admin, False otherwise
+});
+
+const communitySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    date: { type: Date, required: true }, // Date of community creation
+    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }]
 });
 
 const postSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    content: { type: String, required: true },
+    text: { type: String, required: true },
+    image: { type: String }, // String for image url
     date: { type: Date, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
     categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
-    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }]
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
 
 const commentSchema = new mongoose.Schema({
-    content: { type: String, required: true, required: true },
+    text: { type: String, required: true },
+    image: { type: String }, // String for image url
     date: { type: Date, required: true, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true }
 });
 
+// Retrieve from this collection to display posts on forum page
 const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
 });
 
 const tagSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }]
+    name: { type: String, required: true, unique: true },
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    communities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Community' }]
 });
 
+/** Other schemas for website **/
+const teamMemberSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    sID: { type: String, required: true, unique: true },
+    role: { type: String }
+});
 
+/** ------------------------------------------------------------------------------ **/
+/** Define models **/
+const User = mongoose.model('User', userSchema);
+const Community = mongoose.model('Community', communitySchema);
+const Post = mongoose.model('Post', postSchema);
+const Comment = mongoose.model('Comment', commentSchema);
+const Category = mongoose.model('Category', categorySchema);
+const Tag = mongoose.model('Tag', tagSchema);
+const TeamMember = mongoose.model('TeamMember', teamMemberSchema);
+
+module.exports = {
+  User,
+  Community,
+  Post,
+  Comment,
+  Category,
+  Tag,
+  TeamMember
+};
